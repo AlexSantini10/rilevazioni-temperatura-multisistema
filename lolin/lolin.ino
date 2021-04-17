82b67d8ac27b4bce16bbf7900cb9394d10e734df
Alex Santini <47783526+AlexSantini10@users.noreply.github.com>
2021-04-17 12:07:40 +0200

    [update] It works

---
+++ lolin/lolin.ino
@@ -0,0 +1,54 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+
+int outputpin= A0;
+
+#define ssid  "labinformatica"
+#define password  "78945612311"
+
+const uint16_t port = 8080;
+const char *host = "192.168.0.111";
+WiFiClient client;
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Connecting...\n");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid,password); // change it to your ussid and password
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+}
+
+void loop()
+{
+    if (!client.connect(host, port))
+    {
+        Serial.println("Connection to host failed");
+        delay(1000);
+        return;
+    }
+    Serial.println("Sending: ");
+
+    int analogValue = analogRead(outputpin);
+    float millivolts = (analogValue/1024.0) * 3300; //3300 is the voltage provided by NodeMCU
+    float celsius = millivolts/10-7;
+    Serial.print("in DegreeC=   ");
+    Serial.println(celsius);
+
+    String data = String(celsius);
+    
+    client.println(celsius);     // Send data
+    delay(250);
+    while (client.available() > 0)
+    {
+        char c = client.read();
+        Serial.write(c);
+    }
+    Serial.print('\n');
+    client.stop();
+    delay(5000);
+}
